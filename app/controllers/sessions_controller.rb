ff2e02d7962d63061945553760e09c6aa71f40a1
Ron Schnellbacher <rjschnell.tech@sbcglobal.net>
2015-05-07 13:44:00 -0400

    Almost working, login needs changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    sesion[:user_id] = nil
+    session[:user_id] = nil
     redirect_to root_url, notice: t('session.flash.destroy.success')
   end
 

e3de3c8ddf5ffdffaf88a5528eb3348fd7092043
Ron Schnellbacher <rjschnell.tech@sbcglobal.net>
2015-05-07 14:01:55 -0400

    Almost working, require authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  layout 'landing' 
   def new
     @users = User.new
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  layout 'landing'
   def new
     @user = User.new
   end
